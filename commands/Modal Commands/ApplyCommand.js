558173c68986c41e6457b07f4ac969247c403341
skyler.riggle <Skyler.D.Riggle-1@ou.edu>
2023-05-23 11:21:55 -0500

    Removing Limited Modal Creator

--- commands/Modal Commands/ApplyCommand.js
+++ commands/Modal Commands/ApplyCommand.js
@@ -1,30 +1,10 @@
-const { SlashCommandBuilder,
-    ModalBuilder,
-    ActionRowBuilder,
-    TextInputBuilder,
-    TextInputStyle
-} = require('discord.js');
-
-const applicationModal = new ModalBuilder()
-    .setTitle("GDA Officer Application")
-    .setCustomId("officer_modal");
-
-const nameInput = new TextInputBuilder()
-    .setLabel("What is your name?")
-    .setPlaceholder("Enter your name...")
-    .setCustomId('name_input')
-    .setMaxLength(32)
-    .setStyle(TextInputStyle.Short);
-
-const nameAction = new ActionRowBuilder().addComponents(nameInput);
-
-applicationModal.addComponents(nameAction);
+const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('apply')
 		.setDescription('Apply to join the GDA officer team!'),
 	async execute(interaction) {
-        await interaction.showModal(applicationModal);
+
 	},
 };
