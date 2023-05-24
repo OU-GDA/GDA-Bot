d558dc1702cab968d3256977e777fb903cb58314
skyler.riggle <Skyler.D.Riggle-1@ou.edu>
2023-05-24 16:03:42 -0500

    Bootstrap Integration

--- gda-bot/src/index.tsx
+++ gda-bot/src/index.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
+// Global CSS imports
+import 'bootstrap/dist/css/bootstrap.css';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <p>TESTING</p>
   </React.StrictMode>
 );
 
