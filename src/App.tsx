d7f719692ee6f8ea6fcc0e74ec6d521c7c0bf58c
Skyler Riggle <skyler32702@gmail.com>
2023-05-26 08:31:29 -0500

    Style Tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App = () =>
 {
     return (
         <>
-            <NavBar />
+            <NavBar onHamburgerMenu={() => console.log("HAMBURGER!")} />
             
             <BrowserRouter>
                 <Routes>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,21 @@
-const NavBar = () =>
+import Stamp from "../data/images/stamp.webp";
+
+const NavBar = (props: { onHamburgerMenu: () => void }) =>
 {
     return (
         <>
             <div
                 className="position-fixed top-0 w-100 d-flex d-lg-none px-4 align-items-center"
-                style={{ height: '40px', backgroundColor:'#111214' }}
+                style={{ height:'40px', backgroundColor:'#111214', cursor:'pointer' }}
+                onClick={props.onHamburgerMenu}
             >
-                <div className="me-4" style={{width:'18px'}}>
+                <div style={{width:'18px'}}>
                     <div className="bg-light-gray w-100" style={{height:'3px'}} />
                     <div className="bg-light-gray w-100" style={{height:'3px', marginTop:'2px', marginBottom:'2px'}} />
                     <div className="bg-light-gray w-100" style={{height:'3px'}} />
                 </div>
+
+                <img src={Stamp} alt="" className="ms-4 me-1" height={'30px'}/>
 
                 <span>OU GDA BOT | DEVELOPER PORTAL</span>
             </div>
