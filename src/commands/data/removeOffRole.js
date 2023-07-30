b92d9a6a0e935014796b8a35a8412edc9461f1d5
Skyler Riggle <skyler32702@gmail.com>
2023-07-30 17:19:28 -0500

    Edit Officer List

--- src/commands/data/removeOffRole.js
+++ src/commands/data/removeOffRole.js
@@ -1,9 +1,15 @@
 const { SlashCommandBuilder, Interaction } = require('discord.js');
+const DB = require('../../utils/database');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('rmv-officer')
-        .setDescription('Removes an officer role.'),
+        .setDescription('Removes an officer role.')
+        .addIntegerOption(option => option
+            .setName('role-index')
+            .setDescription('The index of the role to be removed. Use off-roles command to see indicies.')
+            .setRequired(true)
+        ),
     
     /**
      * Send the role assignment message
@@ -11,7 +17,9 @@ module.exports = {
      */
 	async execute(interaction)
     {
-        //TODO: Removed Role
+        const index = interaction.options.getInteger('role-index');
+        const roles = DB.get('off_roles') ?? [];
+        DB.set('off_roles', roles.splice(index - 1, 1));
         
         // Reply a success message to the user
         interaction.reply('Role Successfully Removed!');
