26605f3ab47e0c0aa5f6894a990b49ad838889e6
Skyler Riggle <skyler32702@gmail.com>
2023-07-30 18:13:16 -0500

    Netlify Test

---
+++ netlify/functions/index.js
@@ -0,0 +1,100 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const DB = require('../../src/utils/database');
+const {
+    REST,
+    Routes,
+    Client,
+    Events,
+    GatewayIntentBits,
+    Collection
+} = require('discord.js');
+
+exports.handler = async function (event, context) {
+    ///  SETUP  ///////////////////////////////////////////////////////////////////
+
+    // Configure environment variables
+    require('dotenv').config();
+
+    // Create a new client instance
+    const client = new Client({
+        intents: [GatewayIntentBits.Guilds]
+    });
+
+    // Display a success message once logged in
+    client.once(Events.ClientReady, (clientParam) => {
+        console.log(`Client Logged In As: [${clientParam.user.tag}]`);
+    });
+
+    DB.init();
+
+    ///  COMMANDS  ////////////////////////////////////////////////////////////////
+
+    // Create a new empty list of commands
+    const commands = [];
+    client.commands = new Collection();
+
+    // Gather all command files
+    const foldersPath = path.join(__dirname, 'src/commands');
+    const commandFolders = fs.readdirSync(foldersPath);
+
+    // Gather all commands
+    for (const folder of commandFolders)
+    {
+        const commandPath = path.join(foldersPath, folder);
+        const commandFiles = fs.readdirSync(commandPath).filter((file) => file.endsWith('.js'));
+
+        for (const file of commandFiles)
+        {
+            const filePath = path.join(commandPath, file);
+            const command = require(filePath);
+
+            if ('data' in command && 'execute' in command)
+            {
+                commands.push(command.data.toJSON());
+                client.commands.set(command.data.name, command);
+            }
+            else
+            {
+                console.log(`[WARNING] Command at path [${filePath}] is missing data or execute property.`);
+            }
+        }
+    }
+
+    // Set the bots interaction protocol
+    const handleInteraction = require('./src/utils/interactionHandler');
+    client.on(Events.InteractionCreate, handleInteraction);
+
+    // Create a new instance of the REST module
+    const rest = new REST().setToken(process.env.TOKEN);
+
+    /**
+     * Registers bot commands asynchronously 
+     */
+    async function registerCommands()
+    {
+        try
+        {
+            console.log(`Registering ${commands.length} Slash Commands.`);
+
+            const data = await rest.put(
+                Routes.applicationCommands(process.env.APP_ID),
+                {
+                    body: commands
+                }
+            );
+
+            console.log(`Successfully Registered ${data.length} Slash Commands.`)
+        }
+        catch (error)
+        {
+            console.error(error);
+        }
+    }
+
+    // Register the gathered commands
+    registerCommands().then(() => {
+        // Log in to Discord
+        client.login(process.env.TOKEN);
+    });
+};
